5b83524bb66152ed69ff7fce15ddd3c906882885
Scott <scottwesleymusic@gmail.com>
2019-07-08 13:34:08 -0500

    change header image on load

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-// $(document).ready(function () {
+const headerImages = ['../assets/HERO 1.jpg', '../assets/HERO 2.jpg', '../assets/HERO 3.jpg'];
+const hero = document.getElementById('hero-img');
 
-// });
+function changeHeader() {
+    let randomImg = Math.floor(Math.random() * 3);
+    hero.style.backgroundImage = `url("${headerImages[randomImg]}")`;
+    console.log(hero.style.backgroundimage);
+}
 
-
-console.log('hi');
+hero.addEventListener('load', changeHeader())
